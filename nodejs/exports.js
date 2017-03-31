6371932e58d3fbbde081cff4085f55f4aa7af3b5
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2017-03-31 17:06:34 +0200

    nodejs

---
+++ nodejs/exports.js
@@ -0,0 +1,34 @@
+/*
+ *  tijapi: Tiny ImageJ API in JavaScript
+ *  Copyright (C) 2017  Jean-Christophe Taveau.
+ *
+ *  This file is part of tijapi
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with mowgli.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ */
+
+ /*
+  * Use with node.js
+  */
+
+module.exports = {
+  IJ: IJ,
+  ImagePlus : ImagePlus,
+  Measurements : Measurements,
+  wij: this.wij
+};
