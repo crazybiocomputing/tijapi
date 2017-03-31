2f787bf42a6cdc672e307ecfab46549a3d2e6fd1
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2017-03-31 17:05:11 +0200

    Add jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,71 +3,87 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-        options: {
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        },
-        build: {
-            src:  'javascripts/<%= pkg.name %>.js',
-            dest: 'javascripts/<%= pkg.name %>.min.js'
-        }
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src:  'javascripts/es5/<%= pkg.name %>.js',
+        dest: 'javascripts/<%= pkg.name %>_es5.min.js'
+      }
     },
     concat: {
-        dist: {
-            src: ['src/**/*.js'],
-            dest: 'javascripts/<%= pkg.name %>.js'
-        }
+      dist: {
+        src: ['src/**/*.js','nodejs/exports.js'],
+        dest: 'javascripts/<%= pkg.name %>.js'
+      }
     },
-    traceur: {
+    jade: {
+      compile: {
         options: {
-            // traceur options here 
-            experimental: true,
-            // module naming options, 
-            moduleNaming: {
-            stripPrefix: "javascripts/tijapi.js",
-            addPrefix: "edu/crazybiocomputing/tijapi"
-            },
-            copyRuntime: 'javascripts/'
+          pretty: true,
+          data : function(dest, src) {
+            // Return an object of data to pass to templates
+            return require('./jij/menus.json');
+          }
         },
-        custom: {
-            files: [{
-                expand: true,
-                cwd: 'javascripts/',
-                src: ['*.js'],
-                dest: 'javascripts/es5'
-            }]
-        },
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js'],
-      options: {
-        browser:true,
-        strict:false,
-        globals: {
-            jQuery: false,
-            console: true,
-            module: true
+        files: {
+          './jij.html': './jij/jade/jij.jade'
         }
       }
     },
-    jsdoc : {
-        dist : {
-            src: ['src/**/*.js'],
-            options: {
-                configure: 'doc/jsdoc/config.json',
-                destination: 'doc/'
-            }
-        }
+    traceur: {
+      /*
+      options: {
+      // traceur options here
+      experimental: true,
+      // module naming options,
+      moduleNaming: {
+      stripPrefix: "src/js5/tijapi.js",
+      addPrefix: "tijapi"
+    },
+    copyRuntime: 'javascripts/'
+  },
+  */
+  custom: {
+    files: [{
+      expand: true,
+      cwd: 'javascripts/',
+      src: ['tijapi.js'],
+      dest: 'es5'
+    }]
+  },
+},
+jshint: {
+  files: ['Gruntfile.js', 'src/**/*.js'],
+  options: {
+    browser:true,
+    strict:false,
+    globals: {
+      jQuery: false,
+      console: true,
+      module: true
     }
-  });
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-jsdoc');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-traceur');
-  
-  // Default and other tasks.
-  grunt.registerTask('default',['concat','traceur','uglify']);
+  }
+},
+jsdoc : {
+  dist : {
+    src: ['src/**/*.js'],
+    options: {
+      configure: 'doc/jsdoc/config.json',
+      destination: 'doc/'
+    }
+  }
+}
+});
+// These plugins provide necessary tasks.
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+grunt.loadNpmTasks('grunt-jsdoc');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-traceur');
+
+// Default and other tasks.
+grunt.registerTask('default',['concat','traceur','uglify']);
 
 };
-
